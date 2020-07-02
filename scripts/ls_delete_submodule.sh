ecaf73fdbb79367d978eb421586d80119db0ac83
mike lederle <mrlederle@gmail.com>
2020-07-01 23:48:00 -0400

    typo; add mkdir to exercise template

--- scripts/ls_delete_submodule.sh
+++ scripts/ls_delete_submodule.sh
@@ -8,7 +8,7 @@ git submodule deinit -f $path_to_submodule
 echo "completed git submodule deinit -f"
 
 rm -rf .git/modules/$path_to_submodule
-echo "completed rm -rf .git/modules"j
+echo "completed rm -rf .git/modules"
 
 git rm -f $path_to_submodule
 echo "completed git rm -f"
